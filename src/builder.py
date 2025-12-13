ed28f37f5fee237802ed7f60dad5f3192c92e24e
Jens Petter <j.petter111@gmail.com>
2025-12-13 01:20:04 +0000

    Empty home page with banner
    Containing mainly the core setup for page generation by using Jinja2 and Python. Loading in simple header and site title data through JSON and displaying it by inherited templating from Jinja2.

---
+++ src/builder.py
@@ -0,0 +1,39 @@
+import json
+import os
+from jinja2 import Environment, FileSystemLoader
+
+""" Containing functionality to build the website
+    """
+
+env = Environment(loader=FileSystemLoader("templates"))
+
+def load_data():
+    """ Load json data
+    """
+
+    info = json.load(open("data/info.json"))
+    return info
+
+def render(template_name, **args):
+    """ Get a jinja2 template and render it by passing through arguments
+    """
+    template = env.get_template(template_name)
+    return template.render(**args)
+
+def write(path, content):
+    """ Write content to disk
+    """
+
+    path = os.path.join(os.path.dirname(__file__), path)
+    print(path)
+    os.makedirs(os.path.dirname(path), exist_ok=True)
+    with open(path, "w", encoding="utf-8") as f:
+        f.write(content)
+
+def build_site():
+    """ Build the site
+    """
+
+    (info) = load_data()
+    index_html = render("home.html", info=info, Banner=info["Banner"])
+    write("../index.html", index_html)
