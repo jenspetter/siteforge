6a127f84d882169a6f93f406db5b23dd89571bac
Jens Petter <j.petter111@gmail.com>
2026-01-08 15:24:24 +0100

    Introducing adding context to build registry items
    A build registry item can now 'bind' to something in the loaded in content. This gets passed through to Jinja2 templating meaning that a build item can define for example a named context called 'Project' or something which binds to a key value pair match in the loaded in content JSON, which gets assigned and can be used for templating.
    
    This is especially handy in cases such as having 1 single template needing different contexts for that template. Such as a generic re-usable page.

--- src/builder.py
+++ src/builder.py
@@ -6,6 +6,7 @@ from jinja2 import Environment, FileSystemLoader
 BUILD_REGISTRY_PATH = "../data/build_registry.json"
 BUILD_REGISTRY_EXPECTED_VAR_TEMPLATE_NOTATION = "template"
 BUILD_REGISTRY_EXPECTED_VAR_OUTPUT_NOTATION = "output"
+BUILD_REGISTRY_OPTIONAL_VAR_BOUNDCONTEXT_NOTATION = "boundContext"
 
 ASSET_REGISTRY_PATH = "../data/asset_registry.json"
 ASSET_REGISTRY_EXPECTED_VAR_SOURCE_NOTATION = "Source"
@@ -13,7 +14,7 @@ ASSET_REGISTRY_EXPECTED_VAR_DESTINATION_NOTATION = "Destination"
 
 CONTENT_PATH = "../data/content"
 
-""" Containing functionality to build the website
+""" Containing functionality to build a website
     """
 
 env = Environment(loader=FileSystemLoader("templates"))
@@ -39,6 +40,54 @@ def load_asset_registry(path):
     """
 
     return JsonUtils.load_container(path, [ASSET_REGISTRY_EXPECTED_VAR_SOURCE_NOTATION, ASSET_REGISTRY_EXPECTED_VAR_DESTINATION_NOTATION])
+
+def get_processed_content_from_build_item(build_item, content):
+    """
+    Get content based on build registry item notation\n
+    A build registry item can denote a bound context which bounds to something in the loaded in content\n
+    If so, then we try to bind to the context by searching for it in the contents
+    
+    :param build_item: The build item that can potentially contain a bound contexts
+    :param content: The already loaded content
+    """
+
+    # Just return the content if there is no build item
+    if build_item is None:
+        return None
+    
+    processed_content = {}
+
+    # Process bound context if existing
+    if not BUILD_REGISTRY_OPTIONAL_VAR_BOUNDCONTEXT_NOTATION in build_item:
+        return None
+
+    # Try and ressolve all bound context items
+    ressolved_once = False
+    for bound_entry in build_item[BUILD_REGISTRY_OPTIONAL_VAR_BOUNDCONTEXT_NOTATION]:
+        name = bound_entry['Name']
+        # Only supporting 1 query right now
+        query = bound_entry['Where']
+
+        key = query['Key']
+        value = query['Value']
+
+        for k, v in content.items():
+            filtered = JsonUtils.get_filtered_objects(v, JsonUtils.JsonFilter(key, value))
+            if filtered is None:
+                continue
+
+            for item in filtered:
+                # Entry already exists, this is an issue
+                if name in content:
+                    continue
+
+                processed_content[name] = item
+                ressolved_once = True
+    
+    if ressolved_once:
+        return processed_content
+    else:
+        return None
 
 def render(template_name, **args):
     """ Get a jinja2 template and render it by passing through arguments
@@ -74,7 +123,22 @@ def build_site(output_dir):
 
     # Build each registry entry
     for build_entry in build_registry:
-        template = render(build_entry[BUILD_REGISTRY_EXPECTED_VAR_TEMPLATE_NOTATION], **{k.capitalize(): v for k, v in content.items()})
+        final_content = {}
+
+        # Load in existing content
+        for k, v in content.items():
+            final_content[k] = v
+
+        # Process build item
+        processed_content = get_processed_content_from_build_item(build_entry, content)
+
+        # Add processed content if existing
+        if processed_content is not None:
+            for k, v in processed_content.items():
+                final_content[k] = v
+
+        # Render and write
+        template = render(build_entry[BUILD_REGISTRY_EXPECTED_VAR_TEMPLATE_NOTATION], **{k.capitalize(): v for k, v in final_content.items()})
         write(os.path.join(output_dir, build_entry[BUILD_REGISTRY_EXPECTED_VAR_OUTPUT_NOTATION]), template)
  
     # Copy over each asset entry
--- tests/builder_tests.py
+++ tests/builder_tests.py
@@ -60,5 +60,172 @@ class BuilderTests(unittest.TestCase):
         # Loading a file with a missing 'Destination' key. Should return an empty collection
         self.assertEqual(builder.load_asset_registry("../tests/resources/asset_registry/missing_destination_key.json"), {})
 
+    # get_processed_content_from_build_item
+
+    def test_get_processed_content_from_build_item_no_processing_needed(self):
+        content = {}
+        content['Entry'] = {
+            "Id": "Test",
+            "Title": "Cool Title",
+            "Description": "This is the best description!"
+        }
+        
+        build_item_json = {
+            "template": "home.html",
+            "output": "index.html"
+        }
+
+        result = builder.get_processed_content_from_build_item(build_item_json, content)
+        # Expected is nothing as we no build item processing was needed
+        expected = None
+        self.assertEqual(result, expected)
+
+    def test_get_processed_content_from_build_item_simple_bound(self):
+        content = {}
+        content['Entry'] = {
+            "Id": "Test",
+            "Title": "Cool Title",
+            "Description": "This is the best description!"
+        }
+        
+        build_item_json = {
+            "template": "home.html",
+            "output": "index.html",
+            "boundContext": [
+                {
+                    "Name": "BoundEntry",
+                    "Where": {
+                        "Key": "Id",
+                        "Value": "Test"
+                    }
+                }
+            ]
+        }
+
+        result = builder.get_processed_content_from_build_item(build_item_json, content)
+        # Expected is to be bound
+        expected = {}
+        expected['BoundEntry'] = content['Entry']
+        self.assertEqual(result, expected)
+
+    def test_get_processed_content_from_build_item_wrong_key_bound(self):
+        content = {}
+        content['Entry'] = {
+            "Id": "Test",
+            "Title": "Cool Title",
+            "Description": "This is the best description!"
+        }
+        
+        build_item_json = {
+            "template": "home.html",
+            "output": "index.html",
+            "boundContext": [
+                {
+                    "Name": "BoundEntry",
+                    "Where": {
+                        "Key": "IncorrectKey",
+                        "Value": "Test"
+                    }
+                }
+            ]
+        }
+
+        result = builder.get_processed_content_from_build_item(build_item_json, content)
+        # Expected is to not be bound as we bound with the wrong key
+        expected = None
+        self.assertEqual(result, expected)
+
+    def test_get_processed_content_from_build_item_wrong_value_bound(self):
+        content = {}
+        content['Entry'] = {
+            "Id": "Test",
+            "Title": "Cool Title",
+            "Description": "This is the best description!"
+        }
+        
+        build_item_json = {
+            "template": "home.html",
+            "output": "index.html",
+            "boundContext": [
+                {
+                    "Name": "BoundEntry",
+                    "Where": {
+                        "Key": "Id",
+                        "Value": "IncorrectValue"
+                    }
+                }
+            ]
+        }
+
+        result = builder.get_processed_content_from_build_item(build_item_json, content)
+        # Expected is to not be bound as we bound with the wrong value
+        expected = None
+        self.assertEqual(result, expected)
+
+    def test_get_processed_content_from_build_item_complex_bound(self):
+        content = {}
+        content['Entry'] = {
+            "Id": "Test",
+            "Title": "Cool Title",
+            "Description": "This is the best description!"
+        }
+        content['Entry2'] = {
+            "Id": "Test2",
+            "Title": "Cool 2nd Title",
+            "Description": "This is the 2nd best description!"
+        }
+        
+        build_item_json = {
+            "template": "home.html",
+            "output": "index.html",
+            "boundContext": [
+                {
+                    "Name": "BoundEntry",
+                    "Where": {
+                        "Key": "Id",
+                        "Value": "Test"
+                    }
+                }
+            ]
+        }
+
+        result = builder.get_processed_content_from_build_item(build_item_json, content)
+        # Expected is to be bound
+        expected = {}
+        expected['BoundEntry'] = content['Entry']
+        self.assertEqual(result, expected)
+
+    def test_get_processed_content_from_build_item_depth_bound(self):
+        content = {}
+        content['Entry'] = {
+            "Id": "Test",
+            "Title": "Cool Title",
+            "Description": "This is the best description!",
+            "Person": {
+                "Name": "Friday",
+                "Age": 43
+            }
+        }
+        
+        build_item_json = {
+            "template": "home.html",
+            "output": "index.html",
+            "boundContext": [
+                {
+                    "Name": "Friday",
+                    "Where": {
+                        "Key": "Name",
+                        "Value": "Friday"
+                    }
+                }
+            ]
+        }
+
+        result = builder.get_processed_content_from_build_item(build_item_json, content)
+        # Expected is to be bound
+        expected = {}
+        expected['Friday'] = content['Entry']['Person']
+        self.assertEqual(result, expected)
+
 if __name__ == '__main__':
     unittest.main()
